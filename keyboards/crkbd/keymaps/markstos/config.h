bebad82223830d54c2d634c260dfff4424c408fd
James Taranto <taranto.james@gmail.com>
2024-07-30 15:24:28 +1000

    set master to left

--- keyboards/crkbd/keymaps/markstos/config.h
+++ keyboards/crkbd/keymaps/markstos/config.h
@@ -8,22 +8,21 @@ This is the C configuration file for the keymap
 
 #pragma once
 
-//#define USE_MATRIX_I2C
+// #define USE_MATRIX_I2C
 
 /* Select hand configuration */
 
-// #define MASTER_LEFT
-#define MASTER_RIGHT
+#define MASTER_LEFT
+// #define MASTER_RIGHT
 // #define EE_HANDS
 
-//#define SSD1306OLED
-
+// #define SSD1306OLED
 
 // By default, when holding a dual-function key shortly after tapping it, the
 // tapped key will begin repeating. This is handy for fast typists when typing
 // words with double letters, such as "happy". If you turn this setting ON, it
 // will be counted as a held modifier instead.
-//#define TAPPING_FORCE_HOLD
+// #define TAPPING_FORCE_HOLD
 
 // Customized by markstos
 #define TAPPING_TERM 200
@@ -39,18 +38,18 @@ This is the C configuration file for the keymap
 
 // markstos: not sure if these are correct
 // They are intended to beep and flash during flashing
-#define QMK_LED     D5
+#define QMK_LED D5
 #define QMK_SPEAKER C6
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 // Removed because it's now default
-//#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 // When enabled, typing a mod-tap plus second within term will register as the mod-combo
-// Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold 
+// Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold
 #define PERMISSIVE_HOLD
 
-#define COMBO_COUNT 2 
+#define COMBO_COUNT 2
 
 // Set the COMBO_TERM so low that I won't type the keys one after each other during normal typing.
 // They would have be held together intentionally to trigger this.
@@ -58,6 +57,5 @@ This is the C configuration file for the keymap
 
 // These mostly affect my one-shot Shift key, providing a CapsLock alternative.
 // I want a relatively low timeout, so if I accidentally type "Shift", I can pause just briefly and move on.
-#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
-
+#define ONESHOT_TAP_TOGGLE 3 /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 2000 /* Time (in ms) before the one shot key is released */
