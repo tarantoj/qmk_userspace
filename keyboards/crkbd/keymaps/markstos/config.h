998d6ac51c83927eff76420d759b30e952cceee3
James Taranto <taranto.james@gmail.com>
2024-08-01 20:55:46 +1000

    home row mods

--- keyboards/crkbd/keymaps/markstos/config.h
+++ keyboards/crkbd/keymaps/markstos/config.h
@@ -25,16 +25,16 @@ This is the C configuration file for the keymap
 // #define TAPPING_FORCE_HOLD
 
 // Customized by markstos
-#define TAPPING_TERM 200
+#define TAPPING_TERM 250
 #define TAPPING_TERM_PER_KEY
 // used for Tapping Term on thumb keys
-#define TAPPING_TERM_THUMB 175
+#define TAPPING_TERM_THUMB 125
 
 // If you press a dual-role key, press another key, and then release the
 // dual-role key, all within the tapping term, by default the dual-role key
 // will perform its tap action. If the HOLD_ON_OTHER_KEY_PRESS option is
 // enabled, the dual-role key will perform its hold action instead.
-#define HOLD_ON_OTHER_KEY_PRESS
+// #define HOLD_ON_OTHER_KEY_PRESS
 
 // markstos: not sure if these are correct
 // They are intended to beep and flash during flashing
@@ -47,7 +47,7 @@ This is the C configuration file for the keymap
 
 // When enabled, typing a mod-tap plus second within term will register as the mod-combo
 // Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold
-#define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD
 
 #define COMBO_COUNT 2
 
--- keyboards/crkbd/keymaps/markstos/keymap.c
+++ keyboards/crkbd/keymaps/markstos/keymap.c
@@ -48,6 +48,18 @@ enum custom_layers {
 #define RSE_BSP  LT(_RAISE, KC_BSPC)
 #define RSE_ENT  LT(_RAISE, KC_ENT)
 
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+
 
 // For _RAISE layer
 #define CTL_ESC  LCTL_T(KC_ESC)
@@ -55,19 +67,19 @@ enum custom_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSPC  ,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    , KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- OSM(MOD_LALT),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SEMICOLON ,KC_QUOT ,
+    OSM_LCTL,  HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,                          KC_H,   HOME_J    ,HOME_K  ,HOME_L ,HOME_SCLN ,KC_QUOT ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,OSL_FUN ,
+    OSM_AGR,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                          KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_ESC ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         OSM_LCTL, KC_LGUI, LOW_SPC,   RSE_ENT , KC_BSPC ,OSM_SFT
+                                         KC_LGUI, MO(_LOWER), KC_SPC,   KC_ENT , RSE_BSP ,OSM_SFT
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_LOWER] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+      _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -80,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, KC_DEL , XXXXXXX, KC_UNDS, KC_PLUS, KC_PGUP,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_PIPE,_______ ,
+      _______, KC_DEL , XXXXXXX, KC_UNDS, KC_PLUS, KC_PGUP,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_PIPE, KC_DEL ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_HOME, KC_END , KC_MINS, KC_EQL , KC_PGDN,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_APP ,_______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
